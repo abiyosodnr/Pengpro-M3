215d9cc1811e1cd73b6160a026d351e3b7b1d1f1
Abdullah <boltholt17@gmail.com>
2024-01-01 20:22:56 +0700

    finish algo

--- Pengpro-M3/repeat.go
+++ Pengpro-M3/repeat.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func main(){
+     var inp string
+     var i, total int
+     var stop bool
+     var rest byte
+
+     fmt.Scan(&inp)
+     i = 0
+     stop = false
+     total = 0
+
+     for !stop {
+        if inp[i] == 'a' {
+           total++
+}
+        rest = inp[i]
+        i++
+        stop = rest == '.'     
+}
+      fmt.Println(total)
+}
